a9f08fe4d151b593370e33397c531047db583b96
TiagoCastilhos <tiagocastilhos66@gmail.com>
2018-02-27 19:01:06 -0300

    Commit da Aula de Struct

---
+++ ConsoleApp1/Polegada.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    //Struct = Armazena o valor na memória
+    //Classe = Referencia um objeto que está salvo na memória
+    struct Polegada {
+
+        double polegada;
+
+        public Polegada(double centimetro)
+        {
+            polegada = centimetro * 2.54;
+        }
+
+        public override string ToString()
+        {
+            return polegada.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaStruct {
+    class Program {
+        static void Main(string[] args)
+        {
+            Polegada polegada = new Polegada(10);
+
+            Console.WriteLine(polegada);
+            Console.ReadKey();
+        }
+    }
+}
